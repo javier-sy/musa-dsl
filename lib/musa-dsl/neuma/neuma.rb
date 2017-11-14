094d30e220a81a2665cfab767b919fee3a22450d
Javier Sánchez <javier.sy@gmail.com>
2017-11-14 11:32:19 +0100

    work in progress

--- lib/musa-dsl/neuma/neuma.rb
+++ lib/musa-dsl/neuma/neuma.rb
@@ -1,26 +1,70 @@
 require 'citrus'
 
 module Musa::Neuma
-	Citrus.load File.join(File.dirname(__FILE__), "neuma")
+	def self.register grammar_path
+		Citrus.load grammar_path
+	end
 
-	def self.parse string_or_file
+	def self.parse string_or_file, language: nil, decode_with: nil
+
+		language ||= Neuma
+
 		match = nil
 
 		if string_or_file.is_a? String
-			match = Neuma.parse string_or_file
+			match = language.parse string_or_file
+
 		elsif string_or_file.is_a? File
-			match = Neuma.parse string_or_file.read
+			match = language.parse string_or_file.read
+
 		else
 			raise ArgumentError, 'Only String or File allowed to be parsed'
 		end
 
-		match.value
+		if decode_with
+			match.value.collect { |v| decode_with.decode v }
+		else
+			match.value
+		end
 	end
 
-	def self.load filename
+	def self.parse_file filename, decode_with: nil
 		File.open filename do |file|
-			parse file
+			parse file, decode_with: decode_with
+		end
+	end
+
+	register File.join(File.dirname(__FILE__), "neuma")
+
+	class NeumaDecoder
+		def decode attributes
+			parse attributes
+		end
+
+		def parse attributes
+			raise NotImplementedError
+		end
+	end
+
+	class DifferentialNeumaDecoder < NeumaDecoder
+		def initialize start
+			@last = start
+		end
+
+		def decode attributes
+			element = @last.clone
+
+			action = parse attributes
+
+			element = apply action, on: element
+
+			@last = element
+
+			element.clone
+		end
+
+		def apply action, on:
+			raise NotImplementedError
 		end
 	end
 end
-
--- spec/neuma_spec.rb
+++ spec/neuma_spec.rb
@@ -2,91 +2,80 @@ require 'spec_helper'
 
 require 'musa-dsl'
 
-class Processor
-	def initialize start
-		@last = start
-	end
-
-	def process item
-		element = @last.clone
-
-		attributes = item.split('.')
-
-		action = parse attributes
-
-		element = apply action, on: element
-
-		@last = element
-
-		element.clone
-	end
-
-	def parse attributes
-		raise NotImplementedError
-	end
-
-	def apply action, on:
-		raise NotImplementedError
-	end
-end
-
-class X < Processor
-
-	def initialize scale, start
-		super start
-		@scale = scale
-	end
-
+module Impl
 	def parse _attributes
+		case
+		when _attributes.key?(:attributes)
 
-		attributes = _attributes.clone
+			attributes = _attributes[:attributes].clone 
 
-		command = {}
+			command = {}
 
-		pitch = attributes.shift
+			pitch = attributes.shift
 
-		if pitch && !pitch.empty?
-			if pitch[0] == '+' || pitch[0] == '-'
-				command[:delta_pitch] = pitch.to_i
-			else
-				command[:abs_pitch] = pitch.to_i
-			end
-		end
-
-		velocity = attributes.find { |a| /\A (mp | mf | (\+|\-)?(p+|f+)) \Z/x.match a }
-
-
-		if velocity
-			if velocity[0] == '+' || velocity[0] == '-'
-				command[:delta_velocity] = (velocity[1] == 'f' ? 1 : -1) * (velocity.length - 1) * (velocity[0] + '1').to_i
-			elsif 
-				if velocity[0] == 'm'
-					command[:abs_velocity] = (velocity[1] == 'f') ? 1 : -1
+			if pitch && !pitch.empty?
+				if pitch[0] == '+' || pitch[0] == '-'
+					command[:delta_pitch] = pitch.to_i
 				else
-					command[:abs_velocity] = velocity.length * (velocity[1] == 'f' ? 1 : -1)
+					command[:abs_pitch] = pitch.to_i
 				end
 			end
-				
-			attributes.delete velocity
-		end
 
-		duration = attributes.shift
+			velocity = attributes.find { |a| /\A (mp | mf | (\+|\-)?(p+|f+)) \Z/x.match a }
 
-		if duration
-			if duration[0] == '+' || duration[0] == '-'
-				command[:delta_duration] = duration.to_r
-			
-			elsif duration[0] == '*'
-				command[:factor_duration] = duration[1..-1].to_r
-			
-			else
-				command[:abs_duration] = duration.to_r
+
+			if velocity
+				if velocity[0] == '+' || velocity[0] == '-'
+					command[:delta_velocity] = (velocity[1] == 'f' ? 1 : -1) * (velocity.length - 1) * (velocity[0] + '1').to_i
+				elsif 
+					if velocity[0] == 'm'
+						command[:abs_velocity] = (velocity[1] == 'f') ? 1 : -1
+					else
+						command[:abs_velocity] = velocity.length * (velocity[1] == 'f' ? 1 : -1)
+					end
+				end
+					
+				attributes.delete velocity
 			end
+
+			duration = attributes.shift
+
+			if duration
+				if duration[0] == '+' || duration[0] == '-'
+					command[:delta_duration] = duration.to_r
+				
+				elsif duration[0] == '*'
+					command[:factor_duration] = duration[1..-1].to_r
+				
+				else
+					command[:abs_duration] = duration.to_r
+				end
+			end
+
+			command
+
+		when _attributes.key?(:event)
+
+			nil
+
+		when _attributes.key?(:comment)
+
+			nil
+		else
+			raise RuntimeError, "Not processable data #{_attributes}. Keys allowed are :attributes, :event and :comment"
 		end
+	end
+end	
 
-		puts "X.parse #{_attributes} = #{command}"
+class X < Musa::Neuma::NeumaDecoder
+	include Impl
+end
 
-		command
+class Y < Musa::Neuma::DifferentialNeumaDecoder
+	include Impl
+
+	def initialize 
+		super nil
 	end
 
 	def apply action, on:
@@ -94,31 +83,41 @@ class X < Processor
 	end
 end
 
-
-
-
 RSpec.describe Musa::Neuma do
 
 	context "Neuma parsing" do
-=begin			
-		it "Basic parsing" do
-
-			source = File.read File.join(File.dirname(__FILE__), "neuma_spec.neu")
-
-			elements = (source.split /\n/).collect { |line| line.split('#')[0] }.compact.collect { |line_nc| line_nc.split }.flatten
-
-			p = X.new nil, [1, 1, 1]
-
-			elements = elements.collect { |x| p.process x }
-
-			expect(elements).to eq ["hola"]
-		end
-=end
-
+		p = X.new 
 
 		it "Basic neuma inline parsing" do
-			expect(Musa::Neuma.parse('2.3.4 5.6.7 :evento # comentario 1')).to eq([{ attributes: ["2", "3", "4"] }, { attributes: ["5", "6", "7"] }, { event: "evento" }, { comment: " comentario 1" }])
-			expect(Musa::Neuma.parse('(2 3 4) (7 8 9) { esto es un comando complejo { con { xxx } subcomando  }  { y otro } } # comentario 2')).to eq([{ attributes: ["2", "3", "4"] }, { attributes: ["7", "8", "9"] }, { command: "esto es un comando complejo { con { xxx } subcomando  }  { y otro } " }, { comment: " comentario 2" }])
+			expect(Musa::Neuma.parse('2.3.4 5.6.7 :evento # comentario 1')).to eq(
+				[{ attributes: ["2", "3", "4"] }, { attributes: ["5", "6", "7"] }, { event: "evento" }, { comment: " comentario 1" }])
+
+			expect(Musa::Neuma.parse('(2 3 4) (7 8 9) { esto es un comando complejo { con { xxx } subcomando  }  { y otro } } # comentario 2')).to eq(
+				[{ attributes: ["2", "3", "4"] }, { attributes: ["7", "8", "9"] }, { command: "esto es un comando complejo { con { xxx } subcomando  }  { y otro } " }, { comment: " comentario 2" }])
+		end
+
+		it "Basic neuma inline parsing with decoder" do
+
+			result = Musa::Neuma.parse '0 . +1 2.p 2.1/2.p # comentario 1', decode_with: p
+
+			expect(result[0]).to eq({ abs_pitch: 0 })
+			expect(result[1]).to eq({ })
+			expect(result[2]).to eq({ delta_pitch: 1 })
+			expect(result[3]).to eq({ abs_pitch: 2, abs_velocity: -1 })
+			expect(result[4]).to eq({ abs_pitch: 2, abs_duration: Rational(1,2), abs_velocity: -1 })
+			expect(result[5]).to eq(nil)
+		end
+
+		it "Basic neuma file parsing with decoder" do
+
+			result = Musa::Neuma.parse_file File.join(File.dirname(__FILE__), "neuma_spec.neu"), decode_with: p
+
+			expect(result[0]).to eq({ abs_pitch: 0 })
+			expect(result[1]).to eq({ })
+			expect(result[2]).to eq({ delta_pitch: 1 })
+			expect(result[3]).to eq({ abs_pitch: 2, abs_velocity: -1 })
+			expect(result[4]).to eq({ abs_pitch: 2, abs_duration: Rational(1,2), abs_velocity: -1 })
+			expect(result[5]).to eq(nil)
 		end
 	end
 end
