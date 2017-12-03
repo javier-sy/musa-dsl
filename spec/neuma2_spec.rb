1a0ddef81effadd6c8daef0b3dc1fea90ad5e79d
Javier Sánchez <javier.sy@gmail.com>
2017-12-03 13:56:25 +0100

    work in progress

--- spec/neuma2_spec.rb
+++ spec/neuma2_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Musa::Neuma do
 
 	context "Neuma advanced parsing" do
 
-		it "Basic neuma inline parsing" do
+		it "Basic bracketed neuma inline parsing" do
 			expect(Musa::Neuma.parse('2.3.4 5.6.7 [ 9.8.7 6.5.4 ] :evento # comentario 1')).to eq(
 				[ 	{ attributes: ["2", "3", "4"] }, 
 					{ attributes: ["5", "6", "7"] }, 
@@ -20,7 +20,7 @@ RSpec.describe Musa::Neuma do
 					{ command: "esto es un comando complejo { con { xxx } subcomando  }  { y otro }" } ] )
 		end
 
-		it "Basic neuma inline recursive parsing" do
+		it "Basic bracketed neuma inline recursive parsing" do
 			expect(Musa::Neuma.parse('2.3.4 5.6.7 [ 9.8.7 [5.4.5 6.5.6] 6.5.4 [ 5.4.3]] :evento # comentario 1')).to eq(
 				[ 	{ attributes: ["2", "3", "4"] }, 
 					{ attributes: ["5", "6", "7"] }, 
@@ -30,6 +30,12 @@ RSpec.describe Musa::Neuma do
 						{ attributes: [ "6", "5", "4" ] }, 
 						{ neumas: [ { attributes: ["5", "4", "3"] } ] } ] },
 					{ event: :evento } ] )
+		end
+
+		it "" do
+			result = Musa::Neuma.parse_file File.join(File.dirname(__FILE__), "neuma2_spec.neu"), debug: true
+
+			puts result
 		end
 	end
 end
