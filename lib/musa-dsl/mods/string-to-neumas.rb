e0887b7a0224e3e4678ef4ba9bf5ef0d16bbc786
Javier Sánchez <javier.sy@gmail.com>
2019-01-22 11:39:21 +0100

    added nn shortcut method to string

--- lib/musa-dsl/mods/string-to-neumas.rb
+++ lib/musa-dsl/mods/string-to-neumas.rb
@@ -1,9 +1,15 @@
 require 'musa-dsl/neumalang'
+require 'musa-dsl/generative/generative-grammar'
 
 class String
   def to_neumas(language: nil, decode_with: nil, debug: nil)
     Musa::Neumalang.parse(self, language: language, decode_with: decode_with, debug: debug)
   end
 
+  def to_neumas_to_node(language: nil, decode_with: nil, debug: nil)
+    to_neumas(language: language, decode_with: decode_with, debug: debug).to_node
+  end
+
   alias_method :neu, :to_neumas
+  alias_method :nn, :to_neumas_to_node
 end
