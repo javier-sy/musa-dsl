3049d1b40a6532b15a44c90948c40ebf4df1fa41
Javier Sánchez <javier.sy@gmail.com>
2019-01-23 08:31:37 +0100

    optional blocks in sequencer commands

--- lib/musa-dsl/sequencer/sequencer-dsl.rb
+++ lib/musa-dsl/sequencer/sequencer-dsl.rb
@@ -36,40 +36,54 @@ class Musa::Sequencer
     end
 
     def with(*value_parameters, **key_parameters, &block)
+      block ||= proc {}
+
       _as_context_run block, value_parameters, key_parameters
     end
 
     def now(*value_parameters, **key_parameters, &block)
+      block ||= proc {}
+
       @sequencer.now *value_parameters, **key_parameters do |*value_args, **key_args|
         _as_context_run block, value_args, key_args
       end
     end
 
     def at(*value_parameters, **key_parameters, &block)
+      block ||= proc {}
+
       @sequencer.at *value_parameters, **key_parameters do |*value_args, **key_args|
         _as_context_run block, value_args, key_args
       end
     end
 
     def wait(*value_parameters, **key_parameters, &block)
+      block ||= proc {}
+
       @sequencer.wait *value_parameters, **key_parameters do | *values, **key_values |
         _as_context_run block, values, key_values
       end
     end
 
     def play(*value_parameters, **key_parameters, &block)
+      block ||= proc {}
+
       @sequencer.play *value_parameters, **key_parameters do |*value_args, **key_args|
         _as_context_run block, value_args, key_args
       end
     end
 
     def every(*value_parameters, **key_parameters, &block)
+      block ||= proc {}
+
       @sequencer.every *value_parameters, **key_parameters do |*value_args, **key_args|
         _as_context_run block, value_args, KeyParametersProcedureBinder.new(block).apply(key_args)
       end
     end
 
     def move(*value_parameters, **key_parameters, &block)
+      block ||= proc {}
+
       @sequencer.move *value_parameters, **key_parameters do |*value_args, **key_args|
         _as_context_run block, value_args, key_args
       end
