2aa6b65dffca80e03e9f772836f70e3496b23852
Javier Sánchez <javier.sy@gmail.com>
2019-06-14 11:35:27 +0200

    new inputmidiclock clock

--- lib/musa-dsl/transport.rb
+++ lib/musa-dsl/transport.rb
@@ -3,5 +3,6 @@ require 'musa-dsl/transport/transport'
 require 'musa-dsl/transport/timer'
 
 require 'musa-dsl/transport/input-midi-clock'
+require 'musa-dsl/transport/timer-clock'
 require 'musa-dsl/transport/dummy-clock'
 
